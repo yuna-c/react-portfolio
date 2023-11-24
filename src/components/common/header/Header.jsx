199639bef3bf46da9e8eaaf3d602d26b53cc4f56
yuna <yuna744@gmail.com>
2023-11-24 10:18:33 +0900

    1124 Link/NavLink 주석 추가

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,7 +1,7 @@
 import './Header.scss';
 import { NavLink, Link } from 'react-router-dom';
-// Link  : 링크 기능만
-// NavLink : 누르면 활성화 되게
+// Link  : 링크 기능만( Link 컴포넌트를 사용하면 브라우저의 주소만 바꿀뿐, 페이지를 새로 불러오지는 않는다.)
+// NavLink : 누르면 활성화 되게(리액트 웹의 현재 URL과 to가 가리키는 링크가 일치할 때, activeStyle과 activeClassName이 활성화 되고 일치하지 않으면 비활성화)
 export default function Header() {
 	return (
 		<header className='header'>
