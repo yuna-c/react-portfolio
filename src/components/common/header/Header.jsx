9ddf0c14b1ddca2a9f9d8fcac455c1539e0a6874
yuna <yuna744@gmail.com>
2023-11-24 10:14:55 +0900

    1124 activeClassName을 통한 클릭 활성화 기능 설정

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,5 +1,46 @@
 import './Header.scss';
-
+import { NavLink, Link } from 'react-router-dom';
+// Link  : 링크 기능만
+// NavLink : 누르면 활성화 되게
 export default function Header() {
-	return <div className='header'>Header</div>;
+	return (
+		<header className='header'>
+			<h1>
+				<Link to='/'>LOGO</Link>
+			</h1>
+
+			<ul>
+				<li>
+					<NavLink to='/department' activeClassName={'on'}>
+						Department
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/youtube' activeClassName={'on'}>
+						Youtube
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/gallery' activeClassName={'on'}>
+						Gallay
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/community' activeClassName={'on'}>
+						Community
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/members' activeClassName={'on'}>
+						Members
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/members' activeClassName={'on'}>
+						Contant
+					</NavLink>
+				</li>
+			</ul>
+		</header>
+	);
 }
