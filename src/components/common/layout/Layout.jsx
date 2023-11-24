127d1031623f676b684d977edc73ac00e1c7e2c3
yuna <yuna744@gmail.com>
2023-11-24 12:20:37 +0900

    1124 페이지 전환시 모션추가

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,10 +1,17 @@
+import { useEffect, useRef } from 'react';
 import './Layout.scss';
 
 export default function Layout({ children, title }) {
 	console.log(title);
-	//비구조화 할당으로 props값을 가져와서
+	//메인 참조
+	const refFrame = useRef(null);
+	// 컴포넌트 마운트 되자마자 on 붙이기
+	useEffect(() => {
+		refFrame.current.classList.add('on');
+	}, []);
+
 	return (
-		<main className={`layout ${title}`}>
+		<main ref={refFrame} className={`layout ${title}`}>
 			<h1>{title}</h1>
 			<div className='bar'></div>
 			{/* Layout컴포넌트로 감싼 컨텐츠 내용이 아래 children위치에 출력됨 */}
