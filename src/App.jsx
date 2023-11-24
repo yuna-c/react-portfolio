bc9f08df684b844331df00e7a2108c91691b9a87
yuna <yuna744@gmail.com>
2023-11-24 10:03:04 +0900

    1124 Router 각 URL해당되는 컴포넌트 연결 및 browserRouter 변환

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,25 @@ import Department from './components/sub/department/Department';
 import Gallery from './components/sub/gallery/Gallery';
 import Members from './components/sub/members/Members';
 import Youtube from './components/sub/youtube/Youtube';
+import { Route } from 'react-router-dom';
 
-function App() {
+export default function App() {
 	return (
 		<>
 			<Header />
-			<MainWrap />
-			<Department />
-			<Youtube />
-			<Gallery />
-			<Community />
-			<Members />
-			<Contant />
+			{/* 
+			라우터 : 해당 컴포넌트만 보이게
+			<Route path='/'><MainWrap /></Route> 밑에꺼랑 같은고얌!
+			*/}
+			<Route exact path='/' component={MainWrap} />
+			{/* exact : 슬러시 하나일 때만 보이게 하게따  */}
+			<Route path='/department' component={Department} />
+			<Route path='/youtube' component={Youtube} />
+			<Route path='/gallery' component={Gallery} />
+			<Route path='/community' component={Community} />
+			<Route path='/members' component={Members} />
+			<Route path='/contant' component={Contant} />
 			<Footer />
 		</>
 	);
 }
-
-export default App;
