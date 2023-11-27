a90439a11eb4ffd6a7dc0fb36b7021c528916488
yuna <yuna744@gmail.com>
2023-11-27 09:51:22 +0900

    1127 함수 추가

--- src/components/common/layout/Layout.1.jsx
+++ src/components/common/layout/Layout.1.jsx
@@ -5,7 +5,7 @@ export default function Layout({ children, title }) {
 	console.log(title);
 
 	const refFrame = useRef(null); //메인 참조
-
+	const refTitle = useRef(null);
 	/*
 	useEffect 안쪽에서 자주 쓰일만한 특정 함수를 호출해야 함
 	use로 시작하는 커스텀 훅은 특정 함수 안 쪽에서 호출 불가
@@ -22,7 +22,7 @@ export default function Layout({ children, title }) {
 
 	return (
 		<main ref={refFrame} className={`layout ${title}`}>
-			<h1>{title}</h1>
+			<h1 ref={refTitle}>{title}</h1>
 			<div className='bar'></div>
 			{/* Layout컴포넌트로 감싼 컨텐츠 내용이 아래 children위치에 출력됨 */}
 			{children}
