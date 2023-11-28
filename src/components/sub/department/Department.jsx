5c9bae0b0ebee380dfce90227c6712855c0d9551
yuna <yuna744@gmail.com>
2023-11-28 11:39:16 +0900

    1128 title함수함수 기능을 combined함수에 통합

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -6,22 +6,15 @@ import { useCustomText } from '../../../hooks/useText';
 export default function Department() {
 	const [memberTit, setmemberTit] = useState('');
 	const [memberData, setmemberData] = useState([]);
-	const path = useRef(process.env.PUBLIC_URL); //useEffect 의존성 배열 처리위해
-	const changeTitle = useCustomText('title'); //useText의 함수를 반환
+	const path = useRef(process.env.PUBLIC_URL);
+
+	const changeTitle = useCustomText('title');
 	const shortenText = useCustomText('shorten');
 	const combinedTitle = useCustomText('combined');
 
 	const txt1 = 'our-members-score';
 	console.log(combinedTitle(txt1, '-'));
-	// console.log(shortenText(txt1, 4));
-	/*
-	// split : 구분자를 기준점으로 나눠서 배열로 반환(인수 두개 받앙!)
-	const [forward, backward] = txt1.split('-').map((txt) => changeTitle(txt)); // 중괄호 리턴 생략하면 바로 반환
-	// console.log(txt1.split('-'));
-	// console.log(forward);
-	// console.log(backward);
-	// console.log(forward + ' ' + backward);
-  */
+
 	const fetchDepartment = () => {
 		fetch(`${path.current}/DB/department.json`)
 			.then((data) => data.json())
@@ -37,12 +30,7 @@ export default function Department() {
 
 	return (
 		<Layout title={'Deparment'}>
-			<h2>{changeTitle(memberTit)}</h2>
-			{/* 
-      <h2>{shortenText(memberTit, '3')}</h2>
-			<h2>{combinedTitle(memberTit, '-')}</h2> 
-      */}
-			{/* {}=> 연산 필요할 때 */}
+			<h2>{combinedTitle(memberTit, '-')}</h2>
 			<section className='memberBox'>
 				{memberData.map((member, idx) => {
 					return (
@@ -59,25 +47,3 @@ export default function Department() {
 		</Layout>
 	);
 }
-
-/*
-React에서 외부 데이터를 가져와서 화면에 동적으로 출력하는 순서
-1. 외부 데이터를 가져와 담을 빈 state 추가 (보통 빈 배열로 초기화)
-2. fetch문을 이용해서 특정 url의 데이터를 가져온 뒤 동기적으로 배월을 뽑은 후 state에 담아주는 함수 정의
-3. 위에서 만든 함수를 의존성 배열이 비어있는 useEffect문 안쪽에서 호출
-4. state에 담겨있는 data 배열 값을 map으로 반복돌면서 jsx구문 생성
-
-객체의 property에서 key, value값 반복 도는 방법
-const student = {name : 'David', age : 20 }
-//key 반복 돌면서 배열 반환
-Object.keys(student); ['name', 'age'];
-Object.values(student); ['David', 20];
-
-// 문자열 관련 내장 메서드
-전체문자열.charAt(순서) : 전체 문자열에서 해당 순서의 문자값만 반환 
-전체문자열.slice(순서1, 순서2) : 전체 문자열에서 해당 순서1부터 순서2위치까지 문자를 잘라 반환
-전체문자열.toUpperCase() : 전체 문자열을 대문자로 반환
-전체문자열.toLowerCase() : 전체 문자열을 소문자로 반환
-전체문자열.split(구분자) : 전체 문자열을 구분자를 기준으로 나눠서 배열 반환
-전체문자열.join(구분자) : 각 배열값을 구분자로 이어 붙이면서 하나의 문자열로 반환
-*/
--- src/hooks/useText.js
+++ src/hooks/useText.js
@@ -20,12 +20,10 @@ export function useSplitText() {
 
 // 첫글자 잘라서 대문자
 export function useCustomText(type) {
-	if (type === 'title') {
-		//title 걍 구분자임
-		return (txt) => {
-			return txt.charAt(0).toUpperCase() + txt.slice(1);
-		};
-	}
+	const toUpperText = (txt) => {
+		return txt.charAt(0).toUpperCase() + txt.slice(1);
+	};
+
 	// 글자 자르기 함수 추가
 	if (type === 'shorten') {
 		return (txt, len = 100) => {
@@ -39,14 +37,11 @@ export function useCustomText(type) {
 	// 글자 빈칸 추가해서 이어 붙이기
 	if (type === 'combined') {
 		return (txt, spc) => {
-			//txt를 spc 기준으로 나눔
-			// let resultText = '';
-			const resultText = txt // 원본 텍스트를 가져옴
-				.split(spc) // split으로 두번재 인수로 받은 구분자로 분리해서 배열로 반환
-				//.map((data) => (data += data.charAt(0).toUpperCase() + data.slice(1)))
-				.map((data) => data.charAt(0).toUpperCase() + data.slice(1)) // 배열값을 map으로 반복돌며 첫 글자만 대문자로 변환해서 새로운 배열로 반환
-				.join(' '); //새롭게 반환된 배열을 다시 빈칸을 구분자로 해서 하나의 문자열로 이어 붙여줌
-			return resultText; // 위에서 만들어진 문자값을 최종적으로 반환
+			const resultText = txt
+				.split(spc)
+				.map((data) => toUpperText(data))
+				.join(' ');
+			return resultText;
 		};
 	}
 	//글차 빈칸 없이 이어 붙이기
@@ -57,7 +52,7 @@ export function useCustomText(type) {
 			let resultText = '';
 			txt.split(spc).map((data) => {
 				// console.log(data);
-				resultText += data.charAt(0).toUpperCase() + data.slice(1);
+				resultText += toUpperText(data);
 			});
 			return resultText;
 		};
