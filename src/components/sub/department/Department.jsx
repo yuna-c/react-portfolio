5eb6b3af2da65cfe7a6d670e639282a9481bcb86
yuna <yuna744@gmail.com>
2023-11-29 10:49:05 +0900

    1129 history scss 추가

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -37,7 +37,6 @@ export default function Department() {
 				setHistoryLetter(Object.values(json)[0]);
 			});
 	};
-	console.log(HistoryTit, HistoryData, HistoryLetter);
 
 	useEffect(() => {
 		fetchDepartment();
@@ -50,14 +49,21 @@ export default function Department() {
 				<h2>{combinedTitle(HistoryTit)}</h2>
 
 				<div className='con'>
-					{/* History가 반복도는 각각의 데이터 {년도 : 배열} 객체 */}
+					{/* History가 반복도는 각각의 데이터 {년도 : 배열} 객체
+						{2016 : [txt1,txt2,txt3]},
+						{2018 : [txt1,txt2,txt3]},
+						{2020 : [txt1,txt2,txt3]}
+					*/}
 					{HistoryData.map((history, idx) => {
+						console.log(history); //{2016 : [txt1,txt2,txt3]}
+						console.log(Object.keys(history)); // [2016] 0번째 배열값 뽑아서 문자열 '2016'
+						console.log(Object.values(history)); // [[txt1,txt2,txt3]] -> 0번째 배열값 뽑아서 [txt1,txt2,txt3]
 						return (
 							<article key={history + idx}>
 								{/* 현재 반복돌고 있는 객체의 key값을 뽑아서 h3로 출력 :2016 */}
 								<h3>{Object.keys(history)[0]}</h3>
 								<ul>
-									{/* 현재 반복돌고 있는 객체의 value 값을 뽑아서 li로 반복출력 [문자열,문자열] */}
+									{/* 현재 반복돌고 있는 객체의 value 값을 뽑아서 li로 반복출력 [문자열,문자열] 객체(배열안에 배열이다 이거야) */}
 									{Object.values(history)[0].map((list, idx) => {
 										return <li key={list + idx}>{list}</li>;
 									})}
