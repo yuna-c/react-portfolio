4663bbc3de6d36c8ef0bd5744f2f566d650ccb27
yuna <yuna744@gmail.com>
2023-12-07 11:34:19 +0900

    1207 메모글 삭제시 확인 창 띄우기

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -9,58 +9,39 @@ import { useCustomText } from '../../../hooks/useText';
 // https://momentjs.com/
 
 export default function Community() {
-	//--추후에 가져올 시간값에서 -을 .으로 변경하기 위해 combined타입의 텍스트 변환 함수를 텍스트관련 훅으로부터 활성화
 	const changeText = useCustomText('combined');
-
-	//6
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
-		// 로컬저장소 post 키값에 값이 있으면 parsing해서 리턴
 		if (data) return JSON.parse(data);
-		// 없으면 빈 배열 리턴(해당 컴포넌트 제일 처음 호출될 때 한 번)
 		else return [];
 	};
 
-	//1 여기다가 호출
 	const [Post, setPost] = useState(getLocalData()); //함수를 호출해야 하니까 ()
-	//2
 	const refTit = useRef(null);
 	const refCon = useRef(null);
-	// console.log(Post);
 
-	//4 input 초기화 함수
+	// 인풋 초기화
 	const resetPost = () => {
 		refTit.current.value = '';
 		refCon.current.value = '';
 	};
 
-	//3 글 저장 함수
+	// 글 저장 함수
 	const createPost = () => {
-		// 해당 값 없거나 || 해당 값 없거나
 		if (!refTit.current.value.trim() || !refCon.current.value.trim()) {
 			resetPost();
 			return alert('제목과 본문을 모두 입력해 보세요');
 		}
-		// 시간값 받기
 		console.log(new Date());
-
-		//--기존 시간 인스턴스 값을 한국시에 맞게 변경
-		//--new Date().toLocaleString() : 해당 지역시의 표준시로 변환 (단점 : 시간값이 원하지 않는 방향으로 가공됨 '0'사라짐1)
 		const korTime = new Date().getTime() + 1000 * 60 * 60 * 9;
-		//--한국시로 변환된 시간 객체값을 date 키값에 추가로 등록해서 state에 저장
-		// 기존 배열을 통채로 복사할꺼얌(스프레드연산자)
-		// 쓴 순서대로 하려면 객체가 스프레드 연산자보다 먼저와야 해
 		setPost([
 			{ title: refTit.current.value, content: refCon.current.value, date: new Date(korTime).toDateString() },
 			...Post,
 		]);
-		// date: new Date(KorTime) 인스턴스 객체값 근데 에러뜸(리액트 자식노드에 객체로 출력 안된다고 뜨는거 ) 문자가 아닌 객체로 풀린값이 뜨기떄문에 에러가 난다
-		// 그래서 강제로 JSON.stringify(값) 문자화
-		// new Date().toLocaleString()
 		resetPost();
 	};
 
-	//7 글 삭제 함수
+	// 글 삭제 함수
 	const deletePost = (delIndex) => {
 		console.log(delIndex);
 		// 기존의 map과 마찬가지로 기존 배열값을 deep copy해서 새오운 배열 반환
@@ -72,10 +53,11 @@ export default function Community() {
 		// console.log(result);
 		// 2
 		// const result = Post.filter((el, idx) => delIndex !== idx);
+		// ! false = return 실행
+		if (!window.confirm('정말 해당 게시글 삭제?')) return;
 		setPost(Post.filter((_, idx) => delIndex !== idx));
 	};
 
-	//8 검색기능(본문이나 제목에 특정 단어가 있으면 콘솔 뜨게)
 	const filtering = (txt) => {
 		//txt 인수로 받을꺼야
 		const abc = Post.filter((el) => el.title.indexOf(txt) >= 0 || el.content.indexOf(txt) >= 0);
@@ -83,11 +65,8 @@ export default function Community() {
 		alert(abc);
 	};
 
-	//5
 	useEffect(() => {
 		localStorage.setItem('post', JSON.stringify(Post));
-		// JSON.stringify(Post) 강제로 값을 문자화 시키는 구문(빈배열이 문자화 하여)
-		// state변경되기전에 빈 배열값을 저장하기 때문에 초기화되어 값이 사라짐
 	}, [Post]);
 
 	return (
@@ -109,12 +88,7 @@ export default function Community() {
 
 				<div className='showBox'>
 					{Post.map((el, idx) => {
-						//-- 시간값을 getLocalDate 함수를 통해 시간 인스턴스 객체값을 객체상태 그대로 JSX안쪽에 중괄호{}에 넣을 수 없으므로
-						//-- 변환된 시간 객체값을 다시 강제로 문자화
 						const date = JSON.stringify(el.date);
-						// console.log(date);
-						//-- 문자화 시킨 값에서 먼저 T를 기점으로 앞의 시간문자를 찾고 다시 앞의 "를 제외한 나머지 문자 반환(년-월-일)
-						//-- 반환된 문자값을 다시 changeText에 인수로 전달해서 (년.월.일)로 변환
 						const strDate = changeText(date.split('T')[0].slice(1), '.');
 						console.log(strDate);
 						return (
@@ -122,13 +96,7 @@ export default function Community() {
 								<div className='txt'>
 									<h2>{el.title}</h2>
 									<p>{el.content}</p>
-									<span>
-										{/* --변환된 날자값 최종 출력 */}
-										{strDate}
-										{/* {strDate && el.date} */}
-										{/* {el.date && el.date} */}
-										{/* JSON.stringify(el.date) */}
-									</span>
+									<span>{strDate}</span>
 								</div>
 
 								<nav>
@@ -145,31 +113,3 @@ export default function Community() {
 		</Layout>
 	);
 }
-
-/*
-1. input요소로 글입력/출력 박스 생성
-2. 전체글을 관리할 배열 state를 생성 [{글정보1},{글정보2},{글정보3}]
-3. 글입력후 저장버튼 클릭시 글 정보를 객체형태로 state에 계속 추가(create)
-4. state 배열에 추가된 값들을 반복 돌면서 글리스트 출력(Read)
-5. 글 출력시 삭제/수정버튼 추가해서 출력
-6. 글리스트에서 삭제버튼 클릭시 해당 배열 state에서 이벤트 발생한 순번에 객체 제거해서 글 삭제(delete)
-
-C create(데이터 저장) : 글작성
-R	read(데이터 호출) : 글목록 보기
-U update(데이터 변경) : 글 수정
-D delete(데이터 삭제) : 글 삭제
-
-console.log(window);
-Localstorage : 모든 브라우저가 내장하고있는 경량의 저장소
--- 문자값만 저장가능 (5MB)
--- 로컬저장소에 문자열 이외의 값을 저장할 때에는 강제로 문자화 시켜서 저장
--- 로컬저장소의 값을 JS로 가져올 때는 문자값을 반대로 객체화 시켜서 호출 (parcing)	
--- application => Localstorage
-
-Localstorage객체에 활용 가능한 메서드
-- setItem('키' , '문자화된 데이터') : 해당 키값의 데이터를 담아 저장
-- getItem('키') : 해당 키값에 매칭이 되는 데이터를 가져옴
-
-filter() 기능의 원래 쓰임?
-게시글 검색기능
-*/
