0111ed8ff13647c9d823218ac16f9940148c929d
yuna <yuna744@gmail.com>
2023-12-08 12:58:48 +0900

    1208 마커 이미지 변경

--- src/components/sub/contant/Contant.jsx
+++ src/components/sub/contant/Contant.jsx
@@ -6,8 +6,8 @@ import { useEffect, useRef } from 'react';
 // 위치값 정밀하게 보정 하는 법
 // 기존 구굴지도 위치값 복사한 뒤, 카카오 예제의 클릭한 위치 마커표시 직접해보기에서 해당 코드 붙여넣고, 원하는 지점찍으면 소숫점 12자리뜨는데 그거 붙여넣으면 됨
 export default function Contant() {
-	const mapFrame = useRef(null);
 	const { kakao } = window;
+	const mapFrame = useRef(null);
 	console.log(kakao);
 
 	const mapOption = useRef({
@@ -15,11 +15,18 @@ export default function Contant() {
 		level: 3,
 	});
 
+	const imgSrc = process.env.PUBLIC_URL + `/img/marker1.png`;
+	const imgSize = new kakao.maps.Size(232, 99);
+	const imgOpt = { offset: new kakao.maps.Point(116, 99) }; //+ 왼쪽 이동,
+
 	useEffect(() => {
 		const mapInstance = new kakao.maps.Map(mapFrame.current, mapOption.current);
 		// const posInstance = new kakao.maps.LatLng(33.450701, 126.570667);
+		const markerImageInstance = new kakao.maps.MarkerImage(imgSrc, imgSize, imgOpt);
+
 		const markerInstance = new kakao.maps.Marker({
 			position: mapOption.current.center,
+			image: markerImageInstance,
 		});
 
 		markerInstance.setMap(mapInstance);
