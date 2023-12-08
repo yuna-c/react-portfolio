789c70f67404d1cf3ea7744b5c99b484bb56bdba
yuna <yuna744@gmail.com>
2023-12-08 13:16:06 +0900

    1208 마스터 합치기 및 페이지네이션 기능

--- src/components/sub/community/Num.jsx
+++ src/components/sub/community/Num.jsx
@@ -148,7 +148,7 @@ export default function Community() {
 					})}
 			</nav>
 
-			<div className='wrap'>
+			<div className='comunityWrap'>
 				<div className='inputBox'>
 					<input type='text' placeholder='title' ref={refTit} />
 					<textarea cols='30' rows='3' placeholder='content' ref={refCon}></textarea>
