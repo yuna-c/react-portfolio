ff7424014360c4bba01d9129f02a24efccbaa9a3
yuna <yuna744@gmail.com>
2023-12-08 12:12:47 +0900

    1208 지도 출력 오류문제 해결(기존 reset *에 height:auto지정으로 kakao api가 지도가 들어갈 높이값 인지 불가

--- src/components/sub/contant/Contant.jsx
+++ src/components/sub/contant/Contant.jsx
@@ -1,6 +1,40 @@
 import './Contant.scss';
 import Layout from '../../common/layout/Layout';
+import { useEffect, useRef } from 'react';
+
+// https://developers.kakao.com/console/app
+// https://apis.map.kakao.com/
+/*
+사이트 도메인 연결(플랫폼-> web)
+http://localhost:3000
+https://react-portfolio-wheat-eight.vercel.app
+
+new() 인스턴스 생성자 객체 함수를 통해 복사본 객체를 만든다(여러가지 메서드를 호출 가능)
+cdn 코드 쓰면 껐다 켜보기
+*/
 
 export default function Contant() {
-	return <Layout title={'Contant'}>Contant</Layout>;
+	const { kakao } = window;
+	//5028992d611e7153922e7a04d3d292cb
+
+	const mapFrame = useRef(null);
+	// kakao window 객체 안에 있을꺼야
+
+	// const { html } = window;
+	const mapOption = useRef({
+		center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+		level: 3, //지도의 레벨(확대, 축소 정도)
+	});
+
+	useEffect(() => {
+		// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+		// new kakao.maps.Map(mapFrame.current, mapOption.current);
+		new kakao.maps.Map(mapFrame.current, mapOption.current);
+	}, []);
+
+	return (
+		<Layout title={'Contant'}>
+			<article id='map' ref={mapFrame}></article>
+		</Layout>
+	);
 }
