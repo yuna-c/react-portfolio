b4c9a1ce7b2437b3e6ea77634ee886957f932425
yuna <yuna744@gmail.com>
2023-12-08 12:28:54 +0900

    1208 지도 마커 추가

--- src/components/sub/contant/Contant.jsx
+++ src/components/sub/contant/Contant.jsx
@@ -2,16 +2,6 @@ import './Contant.scss';
 import Layout from '../../common/layout/Layout';
 import { useEffect, useRef } from 'react';
 
-// https://developers.kakao.com/console/app
-// https://apis.map.kakao.com/
-/*
-사이트 도메인 연결(플랫폼-> web)
-http://localhost:3000
-https://react-portfolio-wheat-eight.vercel.app
-
-new() 인스턴스 생성자 객체 함수를 통해 복사본 객체를 만든다(여러가지 메서드를 호출 가능)
-*/
-
 export default function Contant() {
 	const mapFrame = useRef(null);
 	const { kakao } = window;
@@ -33,7 +23,7 @@ export default function Contant() {
 	}, []);
 
 	return (
-		<Layout title={'Contact'}>
+		<Layout title={'Contant'}>
 			<article className='mapBox' ref={mapFrame}></article>
 		</Layout>
 	);
