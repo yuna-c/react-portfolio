3d9a94384e574c0e9391fd6926e7979e765a3dc7
yuna <yuna744@gmail.com>
2023-12-11 10:29:08 +0900

    1211 브라우저 리사이즈시마다 지도위치 가운데 갱신

--- src/components/sub/contant/Contant.jsx
+++ src/components/sub/contant/Contant.jsx
@@ -5,14 +5,20 @@ import { useEffect, useRef, useState } from 'react';
 // https://apis.map.kakao.com/web/sample/addMapClickEventWithMarker/
 // 위치값 정밀하게 보정 하는 법
 // 기존 구굴지도 위치값 복사한 뒤, 카카오 예제의 클릭한 위치 마커표시 직접해보기에서 해당 코드 붙여넣고, 원하는 지점찍으면 소숫점 12자리뜨는데 그거 붙여넣으면 됨
+// 1초 동안 60번 이벤트 발생해(리사이즈, 스크롤, 휠)
+
 export default function Contant() {
 	// const { kakao } = window;
 	const kakao = useRef(window.kakao);
 
 	//화면에 출력될 지도정보 배열의 순번이 담길 state
 	const [Index, setIndex] = useState(0);
+	// 변하지 않는 값은 의존성 배열에 두지 않는 것이 좋다
 	const mapFrame = useRef(null);
+	// 마커 ???
 	const marker = useRef(null);
+	// 지도 중간에 두기 위해 참조
+	const mapInstance = useRef(null);
 
 	//지점마다 출력할 정보를 개별적인 객체로 묶어서 배열로 그룹화
 	const mapInfo = useRef([
@@ -49,12 +55,17 @@ export default function Contant() {
 		),
 	});
 
+	// 지도 중심 좌표 (지도 이동시키기)
+	const setCenter = () => mapInstance.current.setCenter(mapInfo.current[Index].latlng);
+
 	useEffect(() => {
-		const mapInstance = new kakao.current.maps.Map(mapFrame.current, {
+		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, {
 			center: mapInfo.current[Index].latlng,
 			level: 3,
 		});
-		marker.current.setMap(mapInstance);
+		marker.current.setMap(mapInstance.current);
+		window.addEventListener('resize', setCenter);
+		return () => window.removeEventListener('resize', setCenter);
 	}, [Index]);
 
 	return (
