38f7f65a1014817eed03b3a88c6ef7103bc29d9a
yuna <yuna744@gmail.com>
2023-12-12 11:57:54 +0900

    메인용 컴포넌트 추가

--- src/components/main/mainWrap/MainWrap.jsx
+++ src/components/main/mainWrap/MainWrap.jsx
@@ -1,6 +1,13 @@
 import './MainWrap.scss';
 import Layout from '../../common/layout/Layout';
+import Visual from '../visual/Visual';
+import Info from '../info/Info';
 
 export default function MainWrap() {
-	return <Layout title={'mainWrap'}>MainWrap</Layout>;
+	return (
+		<Layout title={'mainWrap'}>
+			<Visual />
+			<Info />
+		</Layout>
+	);
 }
