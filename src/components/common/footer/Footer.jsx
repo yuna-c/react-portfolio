c78641fed3d737b31662508f1950bfe285bdeaef
yuna <yuna744@gmail.com>
2023-12-14 21:38:32 +0900

    1214 footer에서 전역 데이터 호출

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,14 +1,18 @@
 import './Footer.scss';
 import { FaFacebookF, FaTwitter, FaYoutube, FaGrinStars } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 //npm i react-icons 아이콘 설치
 //https://react-icons.github.io/react-icons/
 
 export default function Footer() {
+	const { name, position } = useSelector(store => store.memberReducer.members[0]);
 	return (
 		<footer className='footer'>
 			<h1>LOGO</h1>
 			<p>2023 Dcodelab &copy; All Rights Reserved.</p>
-
+			<p>
+				{position}:{name}
+			</p>
 			<ul>
 				<li>
 					<FaFacebookF
