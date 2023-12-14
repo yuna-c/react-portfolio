f50e82f64910b6f89c83b526a3d3484a72f44bc9
yuna <yuna744@gmail.com>
2023-12-14 12:42:19 +0900

    1214 외부 비동기데이터(서버사이드데이터)를 전역 store에 저장할떄 dispatch와 action객체가 필요한 이유

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,17 @@
 import { combineReducers } from 'redux';
 
+/*
+--dispatch가 필요한 이유
+원래 데이터는 자체 DB던, 외부 API데이터던 어쨌던 fetchin을 통해 외부데이터를 가져와야함
+위와같이 reducer 안쪽에 초기 데이터를 설정하는 것이 불가능
+
+--dispatch로 초기 외부 데이터 fetching 후 전역 state에 담는 순서
+1. 컴포넌트 안쪽에서 useEffect로 mount시 fetching함수 호출 후 데이터 반환
+2. 해당 데이터를 지역state에 담는 것이 아닌 action 객체의 payload 값에 담아 dispatch로 reducer에 전달
+3. 아래 리듀서 함수 로직에 의해서 fetching된 데이터 객체가 store에 전달되고
+4. 이후 각 컴포넌트에서 useSelector로해당 데이터에 자유롭게 접근 가능
+*/
+
 const initMember = {
 	members: [
 		{
