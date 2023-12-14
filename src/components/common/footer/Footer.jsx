a30c8a6119573e618dc42a74d35e825ae1474ecf
yuna <yuna744@gmail.com>
2023-12-14 21:47:33 +0900

    1214redux에서 비동기 데이터가 전달되는 순서 및 흐름 주석 정리

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 //https://react-icons.github.io/react-icons/
 
 export default function Footer() {
+	//순서5 - 전역 store값을 useSelector바로 호출 가능
 	const MemberData = useSelector(store => store.memberReducer.members);
 	console.log(MemberData);
 
@@ -12,6 +13,8 @@ export default function Footer() {
 		<footer className='footer'>
 			<h1>LOGO</h1>
 			<p>2023 Dcodelab &copy; All Rights Reserved.</p>
+			{/* 아래 코드에서 조건문을 쓴 이유 */}
+			{/* 첫번째 렌더링시에는 store부터 빈 배열이 전달되므로 두번째 렌더링부터 해당 구문이 실행되도록 조건문 처리 */}
 			<p>{MemberData && `${MemberData[0].position}:${MemberData[0].name}`}</p>
 			<ul>
 				<li>
