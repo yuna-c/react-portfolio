cf9468d12f9719865c6597a76a4b87973f6eb306
yuna <yuna744@gmail.com>
2023-12-18 12:49:10 +0900

    1218 전역객체 루트컴포넌트인 App에 전달

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; //# 빼기
+import { BrowserRouter } from 'react-router-dom';
+//4
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
 	<BrowserRouter>
-		<App />
+		<Provider store={store}>
+			{/* useSelector, Dispatch */}
+			<App />
+		</Provider>
 	</BrowserRouter>,
 	document.getElementById('root')
 );
