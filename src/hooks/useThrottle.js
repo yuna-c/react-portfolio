6f0011cc805443392b1b3ba32bb30a9d0dbab0dc
yuna <yuna744@gmail.com>
2023-12-20 11:05:14 +0900

    1220 test

--- src/hooks/useThrottle.js
+++ src/hooks/useThrottle.js
@@ -3,7 +3,7 @@
 // Debounce : 이벤트 발생하는 간격 시간을 비교해서 일정 시간 간격 안에 이벤트가 발생 중이면 함수 호출을 무기한 연기
 // Throttle : 물리적으로 이벤트 반복 횟수를 줄임
 // Debounce 적용 대표 사례 : 특정 input 요소 입력을 끝날 때 까지 fetching함수 호출 자체를 계속 미룰 때
-// Throttle 적용 대표 사례 : window event(scroll, resize) 발생시마다 불필요하게 ㅁ낳이 호출되는 함수의 호출 횟수를 줄일 때
+// Throttle 적용 대표 사례 : window event(scroll, resize) 발생시마다 불필요하게 많이 호출되는 함수의 호출 횟수를 줄일 때
 import { useRef } from 'react';
 
 // setTimeout이 호출되면 delay뒤에 리턴 값이 반환이 아니라 호출 즉시 return 반환
