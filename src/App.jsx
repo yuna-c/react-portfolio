4ca8322904977b19a963cf8f849246d045e34463
yuna <yuna744@gmail.com>
2023-12-20 12:55:54 +0900

    1220 App에서 액션객체 생성함수 호출해서 바로 dispatch로 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './globalStyles/Reset.scss';
 import './globalStyles/Variables.scss';
 import { Route } from 'react-router-dom';
@@ -16,10 +16,23 @@ import Welcome from './components/sub/members/Welcome';
 import Menu from './components/common/menu/Menu';
 import Detail from './components/sub/youtube/Detail';
 import Num from './components/sub/num/Num';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchYoutube } from './redux/youtubeSlice';
+import { fetchMember } from './redux/memberSlice';
+import { fetchHistory } from './redux/historySlice';
 
 export default function App() {
+	const dispatch = useDispatch();
+	useSelector(store => console.log(store));
+
 	const [Dark, setDark] = useState(false);
 	const [Toggle, setToggle] = useState(false);
+
+	useEffect(() => {
+		dispatch(fetchYoutube());
+		dispatch(fetchMember());
+		dispatch(fetchHistory());
+	}, [dispatch]);
 
 	return (
 		<div className={`wrap ${Dark ? 'dark' : ''} ${useMedia()}`}>
