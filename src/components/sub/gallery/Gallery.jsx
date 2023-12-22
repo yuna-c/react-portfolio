f8f6fef4f8e410faf6b08b3bdacc24e50717fdbc
yuna <yuna744@gmail.com>
2023-12-22 10:24:51 +0900

    1222 메모리 누수현상 해결

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 import Masonry from 'react-masonry-component';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
@@ -9,82 +9,80 @@ import * as types from '../../../redux/action';
 
 export default function Gallery() {
 	const dispatch = useDispatch();
-
-	const myID = useRef('199645532@N06');
+	const myID = useRef('197119297@N02');
 	const isUser = useRef(myID.current);
 	const refNav = useRef(null);
 	const refFrameWrap = useRef(null);
 	const searched = useRef(false);
+	const gap = useRef(20);
+
 	const [Pics, setPics] = useState([]);
 	const [Index, setIndex] = useState(0);
-	const gap = useRef(30);
+	const [Mounted, setMounted] = useState(true);
 
-	// 버튼 재클릭 방지
 	const activateBtn = e => {
 		const btns = refNav.current.querySelectorAll('button');
 		btns.forEach(btn => btn.classList.remove('on'));
 		e && e.target.classList.add('on');
 	};
-
-	// 이벤트 빼기
 	const handleInterest = e => {
 		if (e.target.classList.contains('on')) return;
 		isUser.current = '';
 		activateBtn(e);
 		fetchFlickr({ type: 'interest' });
 	};
-
 	const handleMine = e => {
 		if (e.target.classList.contains('on') || isUser.current === myID.current) return;
 		isUser.current = myID.current;
 		activateBtn(e);
 		fetchFlickr({ type: 'user', id: myID.current });
 	};
-
 	const handleUser = e => {
 		if (isUser.current) return;
 		isUser.current = e.target.innerText;
 		activateBtn();
 		fetchFlickr({ type: 'user', id: e.target.innerText });
 	};
-
 	const handleSearch = e => {
 		e.preventDefault();
 		isUser.current = '';
 		activateBtn();
-		console.log(e);
 		const keyword = e.target.children[0].value;
 		if (!keyword.trim()) return;
-		console.log(keyword);
 		e.target.children[0].value = '';
 		fetchFlickr({ type: 'search', keyword: keyword });
+		//검색함수가 한번이라도 실행되면 영구적으로 초기값을 true로 변경처리
 		searched.current = true;
 	};
+	const fetchFlickr = useCallback(
+		async opt => {
+			const num = 500;
+			const flickr_api = process.env.REACT_APP_FLICKR_API;
+			const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
+			const method_interest = 'flickr.interestingness.getList';
+			const method_user = 'flickr.people.getPhotos';
+			const method_search = 'flickr.photos.search'; //search method 추가
+			const interestURL = `${baseURL}${method_interest}`;
+			const userURL = `${baseURL}${method_user}&user_id=${opt.id}`;
+			const searchURL = `${baseURL}${method_search}&tags=${opt.keyword}`; //search url 추가
+			let url = '';
+			opt.type === 'user' && (url = userURL);
+			opt.type === 'interest' && (url = interestURL);
+			opt.type === 'search' && (url = searchURL);
+			const data = await fetch(url);
+			const json = await data.json();
 
-	const fetchFlickr = async opt => {
-		// console.log('fetching again');
-		const num = 50;
-		const flickr_api = process.env.REACT_APP_FLICKR_API;
-		const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
-		const method_interest = 'flickr.interestingness.getList';
-		const method_user = 'flickr.people.getPhotos';
-		const method_search = 'flickr.photos.search';
-		const interestURL = `${baseURL}${method_interest}`;
-		const userURL = `${baseURL}${method_user}&user_id=${opt.id}`;
-		const searchURL = `${baseURL}${method_search}&tags=${opt.keyword}`;
-		let url = '';
-		opt.type === 'user' && (url = userURL);
-		opt.type === 'interest' && (url = interestURL);
-		opt.type === 'search' && (url = searchURL);
-		const data = await fetch(url);
-		const json = await data.json();
-		setPics(json.photos.photo);
-	};
+			Mounted && setPics(json.photos.photo);
+		},
+		[Mounted]
+	);
 
 	useEffect(() => {
 		refFrameWrap.current.style.setProperty('--gap', gap.current + 'px');
-		fetchFlickr({ type: 'user', id: myID.current });
-	}, []);
+		//fetchFlickr({ type: 'user', id: myID.current });
+		fetchFlickr({ type: 'interest' });
+		return () => setMounted(false);
+	}, [fetchFlickr]);
 
 	return (
 		<>
@@ -98,7 +96,8 @@ export default function Gallery() {
 					</nav>
 
 					<form onSubmit={handleSearch}>
-						<input type='text' placeholder='search' />
+						<input type='text' placeholder='Search' />
+
 						<button className='btnSearch'>
 							<LuSearch />
 						</button>
@@ -108,7 +107,7 @@ export default function Gallery() {
 				<section className='frameWrap' ref={refFrameWrap}>
 					<Masonry className={'frame'} options={{ transitionDuration: '0.5s', gutter: gap.current }}>
 						{searched.current && Pics.length === 0 ? (
-							<h2>해당 키워드에 대한 검색결과가 없습니다.</h2>
+							<h2>해당 키워드에 대한 검색 결과가 없습니다.</h2>
 						) : (
 							Pics.map((pic, idx) => {
 								return (
@@ -119,7 +118,7 @@ export default function Gallery() {
 												dispatch({ type: types.MODAL.start, payload: true });
 												setIndex(idx);
 											}}>
-											<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={Pics.title} />
+											<img src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`} alt={pic.title} />
 										</div>
 										<h2>{pic.title}</h2>
 
