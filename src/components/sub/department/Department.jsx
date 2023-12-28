5c1466af658fcfeadfc258fb8f8c0a3b95ef8246
yuna <yuna744@gmail.com>
2023-12-28 09:47:02 +0900

    1228 Department 비동기데이터 관리하는 커스텀훅 생성, react-query로 history, department 데이터 관리

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -1,78 +1,54 @@
+import { useRef } from 'react';
 import Layout from '../../common/layout/Layout';
-import { useEffect, useRef, useState } from 'react';
 import './Department.scss';
 import { useCustomText } from '../../../hooks/useText';
+import { useHistoryQuery } from '../../../hooks/useHistoryQuery';
+import { useDepartmentQuery } from '../../../hooks/useDepartmentQuery';
 
 export default function Department() {
-	const [memberTit, setmemberTit] = useState('');
-	const [memberData, setmemberData] = useState([]);
-	const [HistoryTit, setHistoryTit] = useState('');
-	const [HistoryData, setHistoryData] = useState([]);
-
+	const combinedTitle = useCustomText('combined');
 	const path = useRef(process.env.PUBLIC_URL);
 
-	const shortenText = useCustomText('shorten');
-	const combinedTitle = useCustomText('combined');
-
-	const fetchDepartment = () => {
-		fetch(`${path.current}/DB/department.json`)
-			.then((data) => data.json())
-			.then((json) => {
-				setmemberTit(Object.keys(json)[0]);
-				setmemberData(Object.values(json)[0]);
-			});
-	};
-
-	const fetchHistory = () => {
-		fetch(`${path.current}/DB/history.json`)
-			.then((data) => data.json())
-			.then((json) => {
-				setHistoryTit(Object.keys(json)[0]);
-				setHistoryData(Object.values(json)[0]);
-			});
-	};
-
-	useEffect(() => {
-		fetchDepartment();
-		fetchHistory();
-	}, []);
+	const { data: HistoryData, isSuccess: isHistory } = useHistoryQuery();
+	const { data: MemberData, isSuccess: isMenber } = useDepartmentQuery();
 
 	return (
 		<Layout title={'Department'}>
 			<section className='historyBox'>
-				<h2>{combinedTitle(HistoryTit)}</h2>
-
+				<h2>{combinedTitle('History')}</h2>
 				<div className='con'>
-					{HistoryData.map((history, idx) => {
-						return (
-							<article key={history + idx}>
-								<h3>{Object.keys(history)[0]}</h3>
-								<ul>
-									{Object.values(history)[0].map((list, idx) => {
-										return <li key={list + idx}>{list}</li>;
-									})}
-								</ul>
-							</article>
-						);
-					})}
+					{isHistory &&
+						HistoryData.map((history, idx) => {
+							return (
+								<article key={history + idx}>
+									<h3>{Object.keys(history)[0]}</h3>
+									<ul>
+										{Object.values(history)[0].map((list, idx) => {
+											return <li key={list + idx}>{list}</li>;
+										})}
+									</ul>
+								</article>
+							);
+						})}
 				</div>
 			</section>
 
 			<section className='memberBox'>
-				<h2>{combinedTitle(memberTit)}</h2>
+				<h2>{combinedTitle('Members')}</h2>
 
 				<div className='con'>
-					{memberData.map((member, idx) => {
-						return (
-							<article key={member + idx}>
-								<div className='pic'>
-									<img src={`${path.current}/img/${member.pic}`} alt={member.name} />
-								</div>
-								<h3>{member.name}</h3>
-								<p>{member.position}</p>
-							</article>
-						);
-					})}
+					{isMenber &&
+						MemberData.map((member, idx) => {
+							return (
+								<article key={member + idx}>
+									<div className='pic'>
+										<img src={`${path.current}/img/${member.pic}`} alt={member.name} />
+									</div>
+									<h3>{member.name}</h3>
+									<p>{member.position}</p>
+								</article>
+							);
+						})}
 				</div>
 			</section>
 		</Layout>
--- src/components/sub/members_/person/Person.jsx
+++
@@ -1,9 +0,0 @@
-import './Person.scss';
-
-export default function Person() {
-	return (
-		<div className='person' style={{ backgroundColor: 'hotpink' }}>
-			Person
-		</div>
-	);
-}
