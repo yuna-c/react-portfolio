f7d24ed082ae4ec4b3551f627549d4ba852b5e02
yuna <yuna744@gmail.com>
2023-12-28 09:52:55 +0900

    1228 react-query로 youtube 데이터 관리

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,76 +1,44 @@
-import { useCustomText } from '../../../hooks/useText';
 import Layout from '../../common/layout/Layout';
 import './Youtube.scss';
-import { useState, useEffect } from 'react';
+import { useCustomText } from '../../../hooks/useText';
 import { Link } from 'react-router-dom';
+import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 
 export default function Youtube() {
-	// 커스텀 훅은 윗단에
 	const customText = useCustomText('combined');
 	const shortenText = useCustomText('shorten');
-
-	const [Vids, setVids] = useState([]);
-	console.log(Vids);
-
-	// promise then 구문을 async await 변경하기 위한 조건 2가지
-	// 조건 1 - promise반환 함수를 감싸주는 wrapping 함수 필요(async), 데이터 묶어야 되니까
-	// 조건 2 - await문은 promise then반환 함수(fetch)에만 지정 가능
-	const fetchYoutube = async () => {
-		const api_key = process.env.REACT_APP_YOUTUBE_API;
-		const pid = process.env.REACT_APP_YOUTUBE_LIST;
-		const num = 10;
-		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
-		/* 중괄호 리턴을 빼거나 리턴을 넣을려면 중괄호까지 넣거나
-		fetch(baseURL)
-			.then((data) => {
-				return data.json();
-			})
-			.then((json) => {
-				setVids(json.items);
-				console.log(Vids);
-			});
-		*/
-		try {
-			const data = await fetch(baseURL); //동기화(데이터라는 값을 받아야 하나끼 먼저 실행하지 않게 변수처리)
-			const json = await data.json(); // await로 동기화해서 배열을 담아옴
-			setVids(json.items);
-			console.log(Vids);
-		} catch (err) {
-			console.error(err);
-		}
-	};
-
-	useEffect(() => {
-		fetchYoutube();
-	}, []);
+	const { data: Vids, isSuccess, isError, error, isLoading } = useYoutubeQuery();
+	console.log(isError);
+	console.log(error);
 
 	return (
 		<Layout title={'Youtube'}>
-			{Vids.map((data, idx) => {
-				const [date, time] = data.snippet.publishedAt.split('T'); //유튜브 업로드 날짜 T를 기점으로 잘라서
-				//return <h3 key={idx}>{data.snippet.title}</h3>; 빈 배열은 에러로 안잡아=> 값이 없으면 반복을 안돌리니까
-				return (
-					<article key={data.id}>
-						<h2>{shortenText(data.snippet.title, 50)}</h2>
+			{isLoading && <p>Loading...</p>}
+			{isSuccess &&
+				Vids.map(data => {
+					const [date, time] = data.snippet.publishedAt.split('T');
 
-						<div className='txt'>
-							<p>{shortenText(data.snippet.description, 250)}</p>
-							<div className='infoBox'>
-								<span>{customText(date, '.')}</span>
-								{/* 하이픈 대신 .으로 바꾼다 */}
-								<em>{time.split('Z')[0]}</em>
-								{/* Z 기준으로 맨 앞에 값 가져온다 */}
+					return (
+						<article key={data.id}>
+							<h2>{shortenText(data.snippet.title, 50)}</h2>
+
+							<div className='txt'>
+								<p>{shortenText(data.snippet.description, 250)}</p>
+								<div className='infoBox'>
+									<span>{customText(date, '.')}</span>
+									<em>{time.split('Z')[0]}</em>
+								</div>
 							</div>
-						</div>
 
-						<div className='pic'>
-							<Link to={`/detail/${data.id}`}>
-								<img src={data.snippet.thumbnails.standard ? data.snippet.thumbnails.standard.url : '/img/member1.jpg'} alt={data.snippet.title} />
-							</Link>
-						</div>
-					</article>
-				);
-			})}
+							<div className='pic'>
+								<Link to={`/detail/${data.id}`}>
+									<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+								</Link>
+							</div>
+						</article>
+					);
+				})}
+			{isError && <p>데이터 반환에 실패했습니다.</p>}
 		</Layout>
 	);
 }
---
+++ src/hooks/useYoutubeQuery.js
@@ -0,0 +1,53 @@
+import { useQuery } from '@tanstack/react-query';
+
+const fetchYoutube = async () => {
+	const api_key = process.env.REACT_APP_YOUTUBE_API;
+	const pid = process.env.REACT_APP_YOUTUBE_LIST;
+	const num = 10;
+	const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+
+	try {
+		const response = await fetch(baseURL);
+		const data = await response.json();
+		return data.items;
+	} catch (err) {
+		throw err;
+	}
+};
+
+export const useYoutubeQuery = () => {
+	return useQuery(['fetchYoutube'], fetchYoutube, {
+		refetchOnMount: false,
+		refetchOnWindowFocus: false,
+		cacheTime: 1000 * 60 * 60 * 24,
+		staleTime: 1000 * 60 * 60 * 24,
+		retry: 3
+	});
+};
+
+const fetchYoutubeById = async ({ queryKey }) => {
+	const api_key = process.env.REACT_APP_YOUTUBE_API;
+	const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&id=${queryKey[1]}`;
+
+	// try {
+	// 	const data = await fetch(baseURL);
+	// 	const json = await data.json();
+	// 	return json.items[0].snippet;
+	// } catch (err) {
+	// 	throw err;
+	// }
+
+	const data = await fetch(baseURL);
+	const json = await data.json();
+	return json.items[0].snippet;
+};
+
+export const useYoutubeQueryById = id => {
+	return useQuery(['fetchYoutube', id], fetchYoutubeById, {
+		refetchOnMount: false,
+		refetchOnWindowFocus: false,
+		cacheTime: 1000 * 60 * 60 * 24,
+		staleTime: 1000 * 60 * 60 * 24,
+		retry: 3
+	});
+};
