a47c50a82867d349133037ea7d6962460913e583
yuna <yuna744@gmail.com>
2023-12-28 09:54:12 +0900

    1228 상세페이지 리액트 쿼리 데이터 관리 (refetching되는 이유 해결필요)

--- src/components/sub/youtube/Detail.jsx
+++ src/components/sub/youtube/Detail.jsx
@@ -1,49 +1,31 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useRef } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Detail.scss';
 import { useParams } from 'react-router-dom';
+import { useYoutubeQueryById } from '../../../hooks/useYoutubeQuery';
 
 export default function Detail() {
-	// const data = useParams();
-	// console.log(data);
-
-	// 1. 유튭 페이지에서 썸네일 선택했을 때 버튼처리
-	// 2. state로 객체 함수 만들기 effect로 한번만 받기
-	console.log('re-render');
-	console.log('--------------------------------');
-
+	const refTitle = useRef(null);
 	const { id } = useParams();
-	// console.log(id);
-	const [youtubeData, setYoutubeData] = useState(null);
-	console.log(youtubeData);
-
-	// 중요한 데이터 : useCallback : 화면에 출력할 것을 뿌리는
-	const fetchSingleData = useCallback(async () => {
-		const api_key = process.env.REACT_APP_YOUTUBE_API;
-		// const api_key = 'AIzaSyBgRldfomRBMNoipsSTKYAmfOarH1iIu8o';
-		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&id=${id}`;
-		const data = await fetch(baseURL);
-		const json = await data.json();
-		setYoutubeData(json.items[0].snippet);
-	}, [id]); // id 값 들어올때만 메모이제이션 풀기
-
-	useEffect(() => {
-		fetchSingleData();
-		console.log(useEffect);
-		//useEffect가 언제 실행되는지
-	}, [fetchSingleData]);
+	const { data: YoutubeData, isSuccess, isError, error, isLoading } = useYoutubeQueryById(id);
+	console.log(YoutubeData, isSuccess, error);
 
 	return (
 		<Layout title={'Detail'}>
-			{youtubeData && (
+			{isLoading && <p>Loading...</p>}
+
+			{/* Optional Chaining : 객체명?.property 해당객체에 값이 없을땐 무시하고 값이 있을때만 property접근 */}
+			<h2 ref={refTitle}>{YoutubeData?.title}</h2>
+			{isSuccess && (
 				<article>
 					<div className='videoBox'>
-						<iframe src={`https://www.youtube.com/embed/${youtubeData?.resourceId.videoId}`} title={youtubeData?.title}></iframe>
+						<iframe src={`https://www.youtube.com/embed/${YoutubeData.resourceId.videoId}`} title={YoutubeData.title}></iframe>
 					</div>
-					<h3>{youtubeData.title}</h3>
-					<p>{youtubeData.description}</p>
+					<h3>{YoutubeData.title}</h3>
+					<p>{YoutubeData.description}</p>
 				</article>
 			)}
+			{isError && <p>데이터 반환에 실패했습니다.</p>}
 		</Layout>
 	);
 }
