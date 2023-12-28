fad890823a0867e4a7695872f89c84ae095ae0a0
yuna <yuna744@gmail.com>
2023-12-28 14:59:37 +0900

    1228 테마 색상 변경시 쿠키에 등록하고 쿠키가 사라지기 전까지 쿠키값 적용

--- src/components/common/themeControl/ThemeControl.jsx
+++ src/components/common/themeControl/ThemeControl.jsx
@@ -3,21 +3,31 @@ import { useCookie } from '../../../hooks/useCookie';
 import './ThemeControl.scss';
 
 export default function ThemeControl() {
-	const { setCookie } = useCookie();
+	const { isCookie, setCookie } = useCookie();
 	const inputEl = useRef(null);
+
+	// 컴포넌트 마운트시 theme라는 이름의 쿠키값이 있으면
+	if (isCookie('theme')) {
+		// 현재 쿠키값에서 'theme=' 다음 문자값을 가져와서 ; 기준으로 배열을 분리 한 뒤 제일 첫번째 값이 theme 컬러값
+		// 해당 쿠키 컬러값으로 자동 세팅
+		document.body.style.setProperty('--pointColor', document.cookie.split('theme=')[1].split(';')[0]);
+	}
+	// 만약 쿠키가 없으면 그냥 css에 등록되어 있는 기본 --pointColor값 활용
 
 	const changeThemeColor = () => {
 		const color = inputEl.current.value;
 		console.log(color);
-		setCookie('theme', color, 20);
+		setCookie('theme', color, 40);
 		console.log(getComputedStyle(document.body).getPropertyValue('--pointColor'));
 		document.body.style.setProperty('--pointColor', color);
 	};
 
+	console.log('Cookie', isCookie);
+
 	return (
 		<nav className='ThemeControl'>
-			<input type='color' ref={inputEl} />
-			<button onClick={changeThemeColor}>Theme Color</button>
+			<input type='color' ref={inputEl} onChange={changeThemeColor} />
+			{/* <button onClick={changeThemeColor}>Theme Color</button> 접근성은 버튼 필수*/}
 		</nav>
 	);
 }
