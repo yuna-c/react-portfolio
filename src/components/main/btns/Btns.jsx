ead2e998c7a5fd8982e93a70fa5ea1c7c391ff2e
yuna <yuna744@gmail.com>
2024-01-04 11:06:12 +0900

    0104 Btns 스타일링

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useRef, useState } from 'react';
 import './Btns.scss';
 
 export default function Btns() {
-	return <ul className='Btns'>Btn</ul>;
+	const num = useRef(4);
+	const [Index, setIndex] = useState(0);
+
+	// useEffect(() => {
+	// 	setTimeout(() => {
+	// 		num.current = document.body.querySelectorAll('.myScroll').length;
+	// 	}, 500);
+	// }, []);
+
+	return (
+		<ul className='Btns'>
+			{Array(num.current)
+				.fill()
+				.map((_, idx) => {
+					return <li key={idx} className={idx === Index ? 'on' : ''}></li>;
+				})}
+		</ul>
+	);
 }
